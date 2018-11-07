1e11f7fdf08baf96e50b0f3e904d35347b4fedb8
Trần Đức Anh <ducanhxom4@gmail.com>
2018-11-07 07:32:49 +0700

    add

--- LoadImageURL/ViewController.swift
+++ LoadImageURL/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    var url = URL(string: "https://www.lawliberty.org/wp-content/uploads/2017/12/AdobeStock_105994137-1024x680.jpeg")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        uRL = NSURL(string: "http://i.imgur.com/w5rkSIj.jpg")
+//        url = URL(string: "https://www.lawliberty.org/wp-content/uploads/2017/12/AdobeStock_105994137-1024x680.jpeg")
+//        let data = try! Data(contentsOf: url!)
+//        myImageView.image = UIImage(data: data)
+//        // Do any additional setup after loading the view, typically from a nib.
+        myImageView.load(url: url!)
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
 
